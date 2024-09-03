d037f94340f417dcf1bfbe82f23f9e1887634c4e
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-02 21:19:57 -0400

    Actualizar owner-add_level.js

--- plugins/owner-add_level.js
+++ plugins/owner-add_level.js
@@ -1,11 +1,5 @@
 import MessageType from '@whiskeysockets/baileys';
 
-function clockString(ms) {
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
-}
 let handler = async (m, { conn, text }) => {
     if (!text) throw 'Masukkan jumlah Limit yang akan diberi'
     let who
