94006f5b7d9767ed2f8a1c123c1182d3b90d49b8
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-02 21:24:10 -0400

    Actualizar owner-add_level.js

--- plugins/owner-add_level.js
+++ plugins/owner-add_level.js
@@ -4,15 +4,15 @@ const handler = async (m, {conn, text}) => {
   let who;
   if (m.isGroup) who = m.mentionedJid[0];
   else who = m.chat;
-  if (!who) throw '*etiqueta 🏷️ ala persona para agregarle sus corazones 🤍*';
+  if (!who) throw '*etiqueta 🏷️ ala persona para agregarle sus niveles 🆙*';
   const txt = text.replace('@' + who.split`@`[0], '').trim();
-  if (!txt) throw 'ingresa la cantidad de corazones 🤍 a agregar';
+  if (!txt) throw 'ingresa la cantidad de niveles 🆙 a agregar';
   if (isNaN(txt)) throw 'no se admiten símbolos solo números 🔢';
   const dmt = parseInt(txt);
   let limit = dmt;
   const pjk = Math.ceil(dmt * pajak);
   limit += pjk;
-  if (limit < 1) throw '*el número mínimo de corazones a agregar es 1 🤍*';
+  if (limit < 1) throw '*el número mínimo de niveles a agregar es 1 🆙*';
   const users = global.db.data.users;
   users[who].level += dmt;
   m.reply(`*🤍 𝐀𝐆𝐑𝐄𝐆𝐀𝐃𝐎*
