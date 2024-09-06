59ac34a4f9ebda96564d310b1c561a5b9d02a3fa
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-05 21:01:35 -0400

    Actualizar plugins

---
+++ plugins/gc-antiBot.js
@@ -0,0 +1,31 @@
+let handler = async (
+	m,
+	{ conn, usedPrefix, command, args, isOwner, isAdmin, isROwner },
+) => {
+	let chat = global.db.data.chats[m.chat];
+
+	if (args[0] === "on") {
+		if (chat.antiBot) {
+			return m.reply("**ANTI BOT**\n_Este comando fue activado con éxito!_");
+		} else {
+			chat.antiBot = true;
+			return m.reply("**ANTI BOT**\n_El comando ya fue activado anteriormente!_");
+		}
+	} else if (args[0] === "off") {
+		if (!chat.antiBot) {
+			return m.reply("**ANTI BOT**\n_El comando se desactivó con éxito!_");
+		} else {
+			chat.antiBot = false;
+			return m.reply("**ANTI BOT**\n_El comando ya fue desactivado anteriormente!_");
+		}
+	} else {
+		return m.reply(`Ejemplo: ${usedPrefix}antibot <on/off>`);
+	}
+};
+
+handler.help = ["antibot *on/off*"];
+handler.tags = ["group"];
+handler.group = true;
+handler.command = ["antibot"];
+
+export default handler;
