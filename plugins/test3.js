b8ba8eaa62d03a6a73e3eee52cfd99f4f42119b1
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-06 11:05:24 -0400

    Actualizar test3.js

--- plugins/test3.js
+++ plugins/test3.js
@@ -1,37 +1,16 @@
-import fs from 'fs';
-const timeout = 60000;
-const poin = 10;
-const handler = async (m, {conn, usedPrefix}) => {
-  conn.tekateki = conn.tekateki ? conn.tekateki : {};
-  const id = m.chat;
-  if (id in conn.tekateki) {
-    conn.reply(m.chat, 'Todavía hay acertijos sin responder en este chat', conn.tekateki[id][0]);
-    throw false;
+let handler = async (m, { conn,setReply }) => {
+  if (!m.quoted) throw "responde a un mensaje de canal";
+  try {
+    let id = (await m.getQuotedObj()).msg.contextInfo.forwardedNewsletterMessageInfo
+      ;
+    let teks = "Nombre:" + " `" + `${id.newsletterName}` + "`\n";
+    teks += "Id:" + " `" + `${id.newsletterJid}` + "`";
+    await conn.reply(m.chat, teks.trim(), m);
+  } catch (e) {
+    console.log(e);
+    throw "Solo debe ser un chat de canal";
   }
-  const tekateki = JSON.parse(fs.readFileSync(`./src/game/acertijo.json`));
-  const json = tekateki[Math.floor(Math.random() * tekateki.length)];
-  const _clue = json.response;
-  const clue = _clue.replace(/[A-Za-z]/g, '_');
-  const caption = `
-ⷮ🚩 *ACERTIJOS*
-✨️ *${json.question}*
-
-⏱️ *Tiempo:* ${(timeout / 1000).toFixed(2)} Segundos
-🎁 *Premio:* *+${poin}* Centavos 🪙`.trim();
-  conn.tekateki[id] = [
-await conn.sendButton(m.chat, caption, 'Quiz🍃', [
-[_clue, _clue],
-["Opción 2"," Opción 2"],
-["Opción 3"," Opción 3"]
-], null, m)
-//await conn.reply(m.chat, caption, m), json,
-    poin,
-    setTimeout(async () => {
-      if (conn.tekateki[id]) await conn.reply(m.chat, `🤍 Se acabó el tiempo!\n*Respuesta:* ${json.response}`, conn.tekateki[id][0]);
-      delete conn.tekateki[id];
-    }, timeout)];
 };
-handler.help = ['acertijo'];
-handler.tags = ['game'];
-handler.command = /^(test3)$/i;
-export default handler;
+handler.help = handler.command = ["ci"];
+handler.tags = ["main"];
+module.exports = handler;
