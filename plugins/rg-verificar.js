ec4c8e3fbc4ac19139b96babe59cda43923c5df5
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-06 21:16:19 -0400

    Actualizar rg-verificar.js

--- plugins/rg-verificar.js
+++ plugins/rg-verificar.js
@@ -7,9 +7,7 @@ let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  if (user.registered === true) return await conn.sendButton(m.chat, `🐈‍⬛ Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg* <Número de serie>`, packname, [
-    ['Autoverificar', `${usedPrefix}reg ${name2}.18`]
-  ], null, m)
+  if (user.registered === true) return m.reply(`🐈‍⬛ Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg* <Número de serie>`)
   if (!Reg.test(text)) return m.reply(`🐈‍⬛ Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.18*`)
   let [_, name, splitter, age] = text.match(Reg)
   if (!name) return m.reply('🐈‍⬛ El nombre no puede estar vacío.')
