80ad949a47e103f8d735aac7ca0363707d30a480
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-08 15:33:49 -0400

    Actualizar dl-tiktok2.js

--- plugins/dl-tiktok2.js
+++ plugins/dl-tiktok2.js
@@ -32,8 +32,9 @@ await m.react('✅')
   }
 }
 handler.help = ['tiktok2 *<link>*']
+handler.corazones = 2
 handler.tags = ['dl']
-handler.command = ['tiktok2']
+handler.command = /^(tiktok2)$/i;
 
 export default handler
 
