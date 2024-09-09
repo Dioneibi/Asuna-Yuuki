b44f3530cfb7f06332786e44a66819d581ec13fd
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-09 19:13:06 -0400

    Actualizar plugins

---
+++ plugins/inteligencia-gemini.js
@@ -0,0 +1,25 @@
+/* By Jtxs 🐢 */     
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) return conn.reply(m.chat, `*Ingresa un texto para hablar con gemini*`, m)
+
+try {
+let api = await fetch(`https://apis-starlights-team.koyeb.app/starlight/gemini?text=${text}`)
+let json = await api.json()
+
+if (json.status === 'true') {
+await conn.reply(m.chat, json.result, m)
+} else {
+conn.reply('error :v')
+}
+} catch {
+conn.reply('error :v')
+}
+}
+
+handler.help = ['gemini <texto>'];
+handler.tags = ['anime'];
+handler.command = ['gemini']
+
+export default handler
