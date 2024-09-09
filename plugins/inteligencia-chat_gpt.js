9254f362ddec9f4e7758e960da85e96a3f55e31b
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-09 19:58:50 -0400

    Actualizar inteligencia-chat_gpt.js

--- plugins/inteligencia-chat_gpt.js
+++ plugins/inteligencia-chat_gpt.js
@@ -9,7 +9,18 @@ let api = await fetch(`https://apis-starlights-team.koyeb.app/starlight/chatgpt?
 let json = await api.json()
 
 if (json.result) {
-await conn.reply(m.chat, json.result, m)
+await conn.sendMessage(m.chat, {
+text: json.result,
+contextInfo: {
+externalAdReply: {
+title: '[ 𝐂 𝐇 𝐀 𝐓 - 𝐆 𝐏 𝐓 ]',
+body: '©𝟐𝟎𝟐𝟒 𝐀𝐧𝐠𝐞𝐥𝐢𝐭𝐨-𝐎𝐅𝐂',
+thumbnailUrl: 'https://i.ibb.co/XjqJjpP/file.jpg',
+sourceUrl: canal,
+mediaType: 1,
+renderLargerThumbnail: true
+}}},
+{ quoted: m})
 } else {
 conn.reply('error :v')
 }
