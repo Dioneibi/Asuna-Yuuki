f389f46a3c651f5a6b7ede741fddef0eaef35dcf
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-09 19:10:23 -0400

    Actualizar plugins

---
+++ plugins/herramientas-transcript_yt.js
@@ -0,0 +1,20 @@
+/* By Jtxs 🐢 */     
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) return conn.reply(m.chat, `*Ingresa un link de youtube*`, m)
+
+try {
+let api = await fetch(`https://apis-starlights-team.koyeb.app/starlight/transcribir-youtube?url=${text}`)
+let json = await api.json()
+
+await conn.reply(m.chat, json.result, m)
+
+} catch {
+conn.reply('error :v')
+}
+}
+
+handler.command = ['transcriptyt']
+
+export default handler
