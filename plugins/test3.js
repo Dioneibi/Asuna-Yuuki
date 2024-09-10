859407035b61d476c393ba3f40886555b6c460d4
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-09 21:08:07 -0400

    Actualizar test3.js

--- plugins/test3.js
+++ plugins/test3.js
@@ -3,7 +3,6 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) throw `Use example ${usedPrefix + command} https://www.facebook.com/watch?v=636541475139*`
 const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
 for (const { url, isVideo } of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `✨ *ᴜʀʟ:* ${url}`, m)
-  throw info
 }
 handler.help = ['facebook2'].map(v => v + ' <url>')
 handler.tags = ['downloader']
