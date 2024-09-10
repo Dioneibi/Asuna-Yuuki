0e56ef37b11246c7ed1e87669b227db8f0deafce
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-09-09 21:38:49 -0400

    Actualizar test3.js

--- plugins/test3.js
+++ plugins/test3.js
@@ -1,7 +1,7 @@
 import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) throw `Use example ${usedPrefix + command} https://www.facebook.com/watch?v=636541475139*`
-const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
+const { result } = await facebookdl(args[0]))
 for (const { url, isVideo } of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `*\`[ FACEBOOK VIDEO ]\`*`, m, null, rcanal)
 }
 handler.help = ['facebook2'].map(v => v + ' <url>')
