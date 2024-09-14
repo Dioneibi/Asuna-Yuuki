4b1e5d7eb3109769b724491837db3c154e295b4d
Dioneibi <ulcidecono@gmail.com>
2024-09-14 14:46:24 -0400

    Update rpg-cofre.js

--- plugins/rpg-cofre.js
+++ plugins/rpg-cofre.js
@@ -8,7 +8,7 @@ const handler = async (m, {isPrems, conn}) => {
   const ai = Math.floor(Math.random() * 4000);
   const expp = Math.floor(Math.random() * 5000);
 
-  global.db.data.users[m.sender].corazones += dia;
+  global.db.data.users[m.sender].Dolares += dia;
   global.db.data.users[m.sender].money += ai;
   global.db.data.users[m.sender].joincount += tok;
   global.db.data.users[m.sender].exp += expp;
@@ -20,7 +20,7 @@ const handler = async (m, {isPrems, conn}) => {
 ╰━━━━━━━━━━━━⬣
 
 ╭━〔 ${global.botname} 〕⬣
-┃ *${dia} Corazones* 🤍
+┃ *${dia} Dolares* 💵
 ┃ *${tok} Tokens* 💰
 ┃ *${ai} GeneCoins* 💸
 ┃ *${expp} Exp* ⚡
