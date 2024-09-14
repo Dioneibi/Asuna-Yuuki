c4dd9fda9c274eb5e028e1955fc0472695e79c87
Dioneibi <ulcidecono@gmail.com>
2024-09-14 18:08:00 -0400

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, text, args, command }) => {
-m.react('✨')
+m.react('🎋')
 
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
 
@@ -8,9 +8,9 @@ let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? c
 let name = await conn.getName(who)
 let edtr = `@${m.sender.split`@`[0]}`
 
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;José Xrl 🍃\nNICKNAME:🌀 José Xrl\nORG:José Xrl 🌀\nTITLE:soft\nitem1.TEL;waid=51902931437:+51 902 931 437\nitem1.X-ABLabel:📞 WhatsApp Owner\nitem2.URL:https://github.com/karim-off/GenesisBot-Pro1\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: agasistencia2@gmail.com\nitem3.X-ABLabel:💌 Correo soporte\nitem4.ADR:;;🇩🇪 Peru;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel: Localización 🫧\nBDAY;value=date:🐈‍⬛ 09-12-2007\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;ANGELITO 🍃\nNICKNAME:🎋 Angel\nORG:Jᴏsᴇ Xʀʟ 🎋\nTITLE:soft\nitem1.TEL;waid=51950148255:+51 950 148 255\nitem1.X-ABLabel:📞 WhatsApp Owner\nitem2.URL:https://github.com/Dioneibi/Asuna-Yuuki\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: tigresitocazador@gmail.com\nitem3.X-ABLabel:💌 Correo soporte\nitem4.ADR:;;🇵🇪 Perú;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel: Localización 🫧\nBDAY;value=date:🎋 10-09-2009\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: packname, contacts: [{ vcard }] }}, { quoted: fkontak })
-let caption = `*👋 Hola ${edtr}, este es el contacto de mi creador*`
+let caption = `*👋 Hola ${edtr}, este es el contacto de mi creador 🎋*`
     await conn.reply(m.chat, caption, tag_own, { mentions: conn.parseMention(caption) })
 
 }
