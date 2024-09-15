613af63d3e68f1b58ff7032d89feb0cde968178b
Dioneibi <ulcidecono@gmail.com>
2024-09-15 11:32:48 -0400

    Update menu-help.js

--- plugins/menu-help.js
+++ plugins/menu-help.js
@@ -1,2 +1,187 @@
-//código incriptado por Jose Xrl
-const _0x3ada7e=_0x2675;(function(_0x2116f9,_0x4ad5f1){const _0x1e0b37=_0x2675,_0x193c23=_0x2116f9();while(!![]){try{const _0x3990b2=parseInt(_0x1e0b37(0xe5))/0x1+parseInt(_0x1e0b37(0x111))/0x2*(parseInt(_0x1e0b37(0xf6))/0x3)+-parseInt(_0x1e0b37(0x110))/0x4*(-parseInt(_0x1e0b37(0xea))/0x5)+parseInt(_0x1e0b37(0x122))/0x6+parseInt(_0x1e0b37(0x129))/0x7*(-parseInt(_0x1e0b37(0x132))/0x8)+parseInt(_0x1e0b37(0x12d))/0x9*(parseInt(_0x1e0b37(0x130))/0xa)+-parseInt(_0x1e0b37(0xd3))/0xb;if(_0x3990b2===_0x4ad5f1)break;else _0x193c23['push'](_0x193c23['shift']());}catch(_0x3b9424){_0x193c23['push'](_0x193c23['shift']());}}}(_0x49cf,0x2ea72));function hi(){const _0x389f0f=_0x2675;console[_0x389f0f(0xe4)](_0x389f0f(0xee));}import _0x585b4d from'ws';let handler=async(_0x37fc51,{conn:_0x23d3f7,usedPrefix:_0x4121c8,text:_0x37c826,args:_0x3d5277,command:_0x42372e})=>{const _0x30139c=_0x2675;let _0x32263f=new Map();global[_0x30139c(0x126)][_0x30139c(0x116)](_0x42066c=>{const _0x43f81e=_0x30139c;_0x42066c[_0x43f81e(0xe7)]&&_0x42066c['ws'][_0x43f81e(0xd2)]&&_0x42066c['ws'][_0x43f81e(0xd2)][_0x43f81e(0xec)]!==_0x585b4d[_0x43f81e(0x112)]&&_0x32263f['set'](_0x42066c[_0x43f81e(0xe7)][_0x43f81e(0x101)],_0x42066c);});let _0x14ef63=[..._0x32263f[_0x30139c(0xd5)]()],_0x299ab3=_0x14ef63[_0x30139c(0x11b)],_0x5c2f3a=Object[_0x30139c(0xd8)](global['db'][_0x30139c(0xf1)][_0x30139c(0xeb)])[_0x30139c(0x11b)],_0x43a405=process[_0x30139c(0x12c)]()*0x3e8,_0x1f2a25=clockString(_0x43a405),_0x195fc2=_0x23d3f7['getName'](_0x37fc51['sender']),_0x25c72d='es',_0x458805=new Date(new Date()+0x36ee80),_0x5d4ebe=_0x458805['toLocaleTimeString'](_0x25c72d,{'hour':_0x30139c(0xdc),'minute':_0x30139c(0xdc),'second':_0x30139c(0xdc)}),_0x55599c=_0x23d3f7[_0x30139c(0xe7)][_0x30139c(0x101)]==global[_0x30139c(0x119)][_0x30139c(0xe7)][_0x30139c(0x101)]?_0x30139c(0x118):_0x30139c(0x109)+(_0x30139c(0x108)+global['conn'][_0x30139c(0xe7)]['jid'][_0x30139c(0xfd)]`@`[0x0]);global[_0x30139c(0x127)]={'key':{'fromMe':![],'participant':_0x30139c(0x10e),'remoteJid':_0x30139c(0xf8)},'message':{'contactMessage':{'displayName':'\x0aʏ\x20ᴜ\x20ᴜ\x20ᴋ\x20ɪ\x20ʙ\x20ᴏ\x20ᴛ','vcard':_0x30139c(0xe1)+_0x195fc2+'\x0aitem1.TEL;waid='+_0x37fc51[_0x30139c(0x11c)][_0x30139c(0xfd)]('@')[0x0]+':'+_0x37fc51[_0x30139c(0x11c)]['split']('@')[0x0]+_0x30139c(0x10b)}}};let _0xb7f06f=Object['keys'](global['db'][_0x30139c(0xf1)]['users'])[_0x30139c(0x11b)],_0x51c973=Object[_0x30139c(0xd5)](global['db'][_0x30139c(0xf1)]['users'])['filter'](_0x357e06=>_0x357e06[_0x30139c(0xff)]==!![])[_0x30139c(0x11b)];_0x37fc51[_0x30139c(0xe9)]('🌸');let _0x598a1e='',_0x3d6c76='';_0x3d6c76+=_0x30139c(0xf5)+_0x30139c(0x104),_0x3d6c76+='`𐚁݁   ݂ 🌸ᩘᮬᅠᩡNᴏᴍʙʀᴇ:`'+_0x30139c(0x131),_0x3d6c76+=_0x30139c(0x120)+('\x20'+fecha+'\x0a'),_0x3d6c76+='`𐚁݁   ݂ 🌸ᩘᮬᅠᩡVᴇʀsɪᴏɴ:`'+('\x20'+vs+'\x0a'),_0x3d6c76+=_0x55599c+'\x0a',_0x3d6c76+='`𐚁݁   ݂ 🌸ᩘᮬᅠᩡPʀᴇғɪᴊᴏ:`'+(_0x30139c(0x11f)+_0x4121c8+'\x20\x20]\x0a'),_0x3d6c76+=_0x30139c(0xde)+('\x20'+(_0x299ab3||'0')+'\x0a'),_0x3d6c76+='`𐚁݁   ݂ 🌸ᩘᮬᅠᩡUsᴜᴀʀɪᴏs:`'+('\x20'+_0x5c2f3a+'\x0a'),_0x3d6c76+=_0x30139c(0x10f)+('\x20'+_0x51c973+'\x0a'),_0x3d6c76+=_0x30139c(0xfe)+('\x20'+_0x1f2a25+'\x0a'),_0x3d6c76+=_0x30139c(0x123),_0x3d6c76+=_0x30139c(0x12f);let _0x5c9e79=[];_0x5c9e79[_0x30139c(0x117)]({'title':_0x30139c(0x10a),'highlight_label':_0x30139c(0xd9),'rows':[{'header':_0x30139c(0x128),'title':'','description':'Verificacion\x20Automáticamente','id':_0x30139c(0xf0)},{'header':'Mᴇɴᴜ\x20Cᴏᴍᴘʟᴇᴛᴏ\x20🌸','title':'','description':_0x30139c(0xe2),'id':_0x30139c(0xf4)},{'header':_0x30139c(0x114),'title':'','description':_0x30139c(0xf3),'id':_0x4121c8+_0x30139c(0x124)},{'header':_0x30139c(0xd0),'title':'','description':'𝘔𝘶𝘦𝘴𝘵𝘳𝘢\x20𝘦𝘭\x20𝘮𝘦𝘯𝘶\x20𝘤𝘢𝘭𝘪𝘦𝘯𝘵𝘦','id':_0x4121c8+_0x30139c(0x12a)},{'header':_0x30139c(0xd7),'title':'','description':'𝘔𝘶𝘦𝘴𝘵𝘳𝘢\x20𝘦𝘭\x20𝘮𝘦𝘯𝘶\x20𝘥𝘦\x20𝘫𝘶𝘦𝘨𝘰𝘴','id':_0x4121c8+_0x30139c(0xfc)},{'header':_0x30139c(0xd6),'title':'','description':'𝘔𝘶𝘦𝘴𝘵𝘳𝘢\x20𝘦𝘭\x20𝘮𝘦𝘯𝘶\x20𝘥𝘦\x20𝘥𝘦𝘴𝘤𝘢𝘳𝘨𝘢𝘴','id':_0x4121c8+_0x30139c(0xf2)},{'header':'Mᴇɴᴜ\x20Aɪ\x20🤖','title':'','description':'𝘔𝘶𝘦𝘴𝘵𝘳𝘢\x20𝘦𝘭\x20𝘮𝘦𝘯𝘶\x20𝘥𝘦\x20𝘐𝘈-𝘉𝘰𝘵','id':_0x4121c8+_0x30139c(0xed)},{'header':_0x30139c(0xe3),'title':'','description':_0x30139c(0x10d),'id':_0x4121c8+_0x30139c(0xef)},{'header':_0x30139c(0xdf),'title':'','description':_0x30139c(0xf7),'id':_0x4121c8+_0x30139c(0x113)},{'header':_0x30139c(0x136),'title':'','description':_0x30139c(0xf9),'id':_0x4121c8+_0x30139c(0xdb)},{'header':_0x30139c(0x121),'title':'','description':_0x30139c(0x103),'id':_0x4121c8+_0x30139c(0x125)}]});let _0x47cd94='https://ibb.co/3NjBHcD',_0x2858e1='https://ibb.co/3NjBHcD',_0x290562=_0x30139c(0xfb),_0x162d8e=_0x30139c(0xe0),_0x47da26=_0x30139c(0xfb),_0x223cdf=_0x30139c(0xe0),_0x4a73d8=_0x30139c(0xfb),_0x2630d2=_0x30139c(0xe0),_0x3141ae=_0x30139c(0xfb),_0x5e2e08=_0x30139c(0xe0),_0x575b3d=_0x30139c(0xfb);await _0x23d3f7[_0x30139c(0x10c)](_0x37fc51['chat'],_0x598a1e,_0x3d6c76,_0x30139c(0x133),[_0x47cd94,_0x2858e1,_0x290562,_0x162d8e,_0x47da26,_0x223cdf,_0x4a73d8,_0x2630d2,_0x3141ae,_0x5e2e08,_0x575b3d][_0x30139c(0x11d)](),_0x5c9e79,estilo);};function _0x2675(_0x5312c3,_0x365646){const _0x49cf26=_0x49cf();return _0x2675=function(_0x2675f9,_0x1585f3){_0x2675f9=_0x2675f9-0xd0;let _0x3d8209=_0x49cf26[_0x2675f9];return _0x3d8209;},_0x2675(_0x5312c3,_0x365646);}handler[_0x3ada7e(0xdd)]=['menu',_0x3ada7e(0x12e),'menú'];export default handler;function _0x49cf(){const _0x589e80=['𝘔𝘶𝘦𝘴𝘵𝘳𝘢\x20𝘭𝘰𝘴\x20𝘨𝘳𝘶𝘱𝘰𝘴\x20𝘥𝘦𝘭\x20𝘣𝘰𝘵','status@broadcast','𝘔𝘶𝘦𝘴𝘵𝘳𝘢\x20𝘭𝘢\x20𝘷𝘦𝘭𝘰𝘤𝘪𝘥𝘢𝘥\x20𝘥𝘦𝘭\x20𝘣𝘰𝘵','map','https://i.ibb.co/nsbttwG/file.jpg','gamemenu','split','`𐚁݁   ݂ 🌸ᩘᮬᅠᩡAᴄᴛɪᴠᴏ:`','registered','join','jid','Bᴜᴇɴᴏs\x20Dɪᴀs\x20❄','𝘔𝘶𝘦𝘴𝘵𝘳𝘢\x20𝘦𝘭\x20𝘮𝘦𝘵𝘰𝘥𝘰\x20𝘥𝘦\x20𝘥𝘰𝘯𝘢𝘳','\x20Angel-ofc\x0a','Bᴜᴇɴᴀs\x20Tᴀʀᴅᴇs\x20🌹','Bᴜᴇɴᴀs\x20Tᴀʀᴅᴇs\x20🥀','Bᴜᴇɴᴀs\x20Nᴏᴄʜᴇs\x20🌌','\x20\x20Wa.me/','`𐚁݁   ݂ 🌸ᩘᮬᅠᩡBᴏᴛ:`\x20Sub\x20-\x20Bot\x20de','🔖\x20SELECCIÓNA\x20LO\x20QUE\x20NECESITES','\x0aitem1.X-ABLabel:Ponsel\x0aEND:VCARD','sendListB','𝘔𝘶𝘦𝘴𝘵𝘳𝘢\x20𝘭𝘢𝘴\x20𝘳𝘦𝘥𝘦𝘴\x20𝘥𝘦𝘭\x20𝘣𝘰𝘵','0@s.whatsapp.net','`𐚁݁   ݂ 🌸ᩘᮬᅠᩡVᴇʀɪғɪᴄᴀᴅᴏs:`','4lPOWip','2MhQQwZ','CLOSED','grupos','Mᴇɴᴜ\x20Aᴜᴅɪᴏs\x20🔊','Bᴜᴇɴᴀs\x20Tᴀʀᴅᴇs\x20🌇','forEach','push','`𐚁݁   ݂ 🌸ᩘᮬᅠᩡBᴏᴛ:`\x20Principal','conn','Bᴜᴇɴᴀs\x20Tᴀʀᴅᴇs\x20🌆','length','sender','getRandom','Bᴜᴇɴᴏs\x20Dɪᴀs\x20🌨','\x20[\x20\x20','`𐚁݁   ݂ 🌸ᩘᮬᅠᩡFᴇᴄʜᴀ:`','Dᴏɴᴀᴄɪᴏ́ɴ\x20Bᴏᴛ\x20☕','2236974euMZxW','`𐚁݁   ݂ 🌸ᩘᮬᅠᩡCʀʀᴀᴅᴏʀ:`\x20#owner\x0a','menuaudios','donate','conns','fcontact','Aᴜᴛᴏ\x20Vᴇʀɪғɪᴄᴀʀ\x20✅','252GsbZTV','labiblia','getHours','uptime','1161asyRUX','help','•*¨*•.¸¸☆*･ﾟ•*¨*•.¸¸☆*･ﾟ•*¨*•.¸¸☆*･ﾟ','8130ppCQjg','\x20Yuuki\x0a','73976bZcCqO','🌸ᛜॕ༹Mᴇɴᴜ\x20Lɪsᴛ🌸ᛜॕ༹','Bᴜᴇɴᴏs\x20Dɪᴀs\x20✨','Bᴜᴇɴᴀs\x20Nᴏᴄʜᴇs\x20🌙','Vᴇʟᴏᴄɪᴅᴀᴅ\x20Bᴏᴛ\x20⚡','Bᴜᴇɴᴏs\x20Dɪᴀs\x20🌄','Mᴇɴᴜ\x20Nsғᴡ\x20🔞','Bᴜᴇɴᴏs\x20Dɪᴀs\x20💫','socket','2136090Vfqdnc','Bᴜᴇɴᴏs\x20Dɪᴀs\x20🌞','values','Mᴇɴᴜ\x20Dᴇsᴄᴀʀɢᴀs\x20📥','Mᴇɴᴜ\x20Jᴇɢᴏs\x20🎮','keys','Popular\x20AsunaYuuki','floor','ping','numeric','command','`𐚁݁   ݂ 🌸ᩘᮬᅠᩡSᴜʙBᴏᴛs:`','Gʀᴜᴘᴏs\x20Asᴜɴᴀ-Yᴜᴜᴋɪ\x20☁️','https://ibb.co/3NjBHcD','BEGIN:VCARD\x0aVERSION:3.0\x0aN:;a,;;;\x0aFN:','MENU\x20COMPLETO','Rᴇᴅᴇs\x20Asᴜɴᴀ-Yᴜᴜᴋɪ\x20🍄','log','87121svLnEP','Bᴜᴇɴᴀs\x20Nᴏᴄʜᴇs\x20🌃','user','Bᴜᴇɴᴏs\x20Dɪᴀs\x20🌅','react','92290mUywzP','users','readyState','menuai','Hello\x20World!','redes','#reg\x20食Nombre.15','data','menudl','𝘔𝘶𝘦𝘴𝘵𝘳𝘢\x20𝘦𝘭\x20𝘮𝘦𝘯𝘶\x20𝘥𝘦\x20𝘢𝘶𝘥𝘪𝘰𝘴','.allmenu','`𐚁݁   ݂ 🌸ᩘᮬᅠᩡCʀᴇᴀᴅᴏʀ:`','404661QkcosZ'];_0x49cf=function(){return _0x589e80;};return _0x49cf();}function clockString(_0x585296){const _0x32d9a6=_0x3ada7e,_0x3b8e6d=Math['floor'](_0x585296/0x36ee80),_0x12490e=Math[_0x32d9a6(0xda)](_0x585296/0xea60)%0x3c,_0x465b2d=Math[_0x32d9a6(0xda)](_0x585296/0x3e8)%0x3c;return console[_0x32d9a6(0xe4)]({'ms':_0x585296,'h':_0x3b8e6d,'m':_0x12490e,'s':_0x465b2d}),[_0x3b8e6d,_0x12490e,_0x465b2d][_0x32d9a6(0xfa)](_0x7e23db=>_0x7e23db['toString']()['padStart'](0x2,0x0))[_0x32d9a6(0x100)](':');}var ase=new Date(),hour=ase[_0x3ada7e(0x12b)]();switch(hour){case 0x0:hour=_0x3ada7e(0x135);break;case 0x1:hour='Bᴜᴇɴᴀs\x20Nᴏᴄʜᴇs\x20💤';break;case 0x2:hour='Bᴜᴇɴᴀs\x20Nᴏᴄʜᴇs\x20🦉';break;case 0x3:hour=_0x3ada7e(0x134);break;case 0x4:hour='Bᴜᴇɴᴏs\x20Dɪᴀs\x20💫';break;case 0x5:hour=_0x3ada7e(0xe8);break;case 0x6:hour=_0x3ada7e(0x137);break;case 0x7:hour=_0x3ada7e(0xe8);break;case 0x8:hour=_0x3ada7e(0xd1);break;case 0x9:hour=_0x3ada7e(0x134);break;case 0xa:hour=_0x3ada7e(0xd4);break;case 0xb:hour=_0x3ada7e(0x11e);break;case 0xc:hour=_0x3ada7e(0x102);break;case 0xd:hour='Bᴜᴇɴᴏs\x20Dɪᴀs\x20🌤';break;case 0xe:hour=_0x3ada7e(0x115);break;case 0xf:hour=_0x3ada7e(0x106);break;case 0x10:hour=_0x3ada7e(0x105);break;case 0x11:hour=_0x3ada7e(0x11a);break;case 0x12:hour=_0x3ada7e(0x135);break;case 0x13:hour=_0x3ada7e(0xe6);break;case 0x14:hour=_0x3ada7e(0x107);break;case 0x15:hour=_0x3ada7e(0xe6);break;case 0x16:hour=_0x3ada7e(0x135);break;case 0x17:hour='Bᴜᴇɴᴀs\x20Nᴏᴄʜᴇs\x20🌃';break;}var greeting=hour;
+import ws from 'ws';
+
+let handler = async (m, { conn, usedPrefix, text, args, command }) => {
+    let uniqueUsers = new Map();
+
+    global.conns.forEach((conn) => {
+        if (conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED) {
+            uniqueUsers.set(conn.user.jid, conn);
+        }
+    });
+
+    let users = [...uniqueUsers.values()];
+    let totalUsers = users.length;
+
+    let totalusr = Object.keys(global.db.data.users).length;
+    let _uptime = process.uptime() * 1000;
+    let uptime = clockString(_uptime);
+    let username = conn.getName(m.sender);
+    let locale = 'es';
+    let d = new Date(new Date + 3600000);
+    let time = d.toLocaleTimeString(locale, {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    });
+
+    let sbot =
+        conn.user.jid == global.conn.user.jid
+        ? "`ʙᴏᴛ ::` Principal"
+        : "`ʙᴏᴛ ::` Sub - Bot de" + `  Wa.me/${global.conn.user.jid.split`@`[0]}`;
+
+    global.fcontact = {
+        key: {
+            fromMe: false,
+            participant: `0@s.whatsapp.net`,
+            remoteJid: "status@broadcast",
+        },
+        message: {
+            contactMessage: {
+                displayName: ``,
+                vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${username}\nitem1.TEL;waid=${m.sender.split("@")[0]}:${m.sender.split("@")[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`,
+            },
+        },
+    };
+
+    let totalreg = Object.keys(global.db.data.users).length;
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length;
+
+    m.react("🍸");
+    let menu = ``;
+
+    let txt = ""
+    txt += '`ᴄʀᴇᴀᴅᴏʀ ::`' + ` Jose Xrl\n`;
+    txt += '`ʙᴏᴛ ::`' + ` Asuna Yuuki\n`;
+    txt += '`ꜰᴇᴄʜᴀ ::`' + ` ${fecha}\n`;
+    txt += '`ᴠᴇʀꜱɪᴏɴ ::`' + ` ${vs}\n`;
+    txt += `${sbot}\n`;
+    txt += '`ᴘʀᴇꜰɪᴊᴏ ::`' + ` [  ${usedPrefix}  ]\n`;
+    txt += '`ᴜꜱᴜᴀʀɪᴏꜱ ::`' + ` ${totalusr}\n`;
+    txt += '`ᴠᴇʀɪꜰɪᴄᴀᴅᴏꜱ ::`' + ` ${rtotalreg}\n`;
+    txt += '`ᴀᴄᴛɪᴠᴏ ::`' + ` ${uptime}\n`;
+    txt += '`ᴄᴏɴᴛᴀᴄᴛᴏ ::` #owner\n\n';
+    txt += "> ʙʏ : Jᴏsᴇ Xʀʟ";
+
+    let listSections = [];
+
+        listSections.push({
+        title: `🔖 SELECCIÓNA LO QUE NECESITES`, highlight_label: `Popular AsunaYuuki`,
+        rows: [
+            {
+                header: "𝗔𝗨𝗧𝗢 𝗩𝗘𝗥𝗜𝗙𝗜𝗖𝗔𝗥 ✅",
+                title: "",
+                description: `Verificacion Automáticamente`,
+                id: `.reg Yuuki.18`,
+            },
+            {
+                header: "𝗠𝗘𝗡𝗨 𝗖𝗢𝗠𝗣𝗟𝗘𝗧𝗢 🌸",
+                title: "",
+                description: `MENU COMPLETO`,
+                id: `.allmenu`,
+            },
+            {
+                header: "𝗠𝗘𝗡𝗨 𝗔𝗨𝗗𝗜𝗢𝗦 🔊",
+                title: "",
+                description: `𝘔𝘶𝘦𝘴𝘵𝘳𝘢 𝘦𝘭 𝘮𝘦𝘯𝘶 𝘥𝘦 𝘢𝘶𝘥𝘪𝘰𝘴`,
+                id: `${usedPrefix}menuaudios`,
+            },
+            {
+                header: "𝗠𝗘𝗡𝗨 𝗡𝗦𝗙𝗪 🔞",
+                title: "",
+                description: `𝘔𝘶𝘦𝘴𝘵𝘳𝘢 𝘦𝘭 𝘮𝘦𝘯𝘶 𝘤𝘢𝘭𝘪𝘦𝘯𝘵𝘦`,
+                id: `${usedPrefix}labiblia`,
+            },
+            {
+                header: "𝗠𝗘𝗡𝗨 𝗝𝗨𝗘𝗚𝗢𝗦 🎮",
+                title: "",
+                description: `𝘔𝘶𝘦𝘴𝘵𝘳𝘢 𝘦𝘭 𝘮𝘦𝘯𝘶 𝘥𝘦 𝘫𝘶𝘦𝘨𝘰𝘴`,
+                id: `${usedPrefix}gamemenu`,
+            },
+            {
+                header: "𝗠𝗘𝗡𝗨 𝗗𝗘𝗦𝗖𝗔𝗥𝗚𝗔𝗦 📥",
+                title: "",
+                description: `𝘔𝘶𝘦𝘴𝘵𝘳𝘢 𝘦𝘭 𝘮𝘦𝘯𝘶 𝘥𝘦 𝘥𝘦𝘴𝘤𝘢𝘳𝘨𝘢𝘴`,
+                id: `${usedPrefix}menudl`,
+            },
+            {
+                header: "𝗠𝗘𝗡𝗨 𝗔𝗜 🤖",
+                title: "",
+                description: `𝘔𝘶𝘦𝘴𝘵𝘳𝘢 𝘦𝘭 𝘮𝘦𝘯𝘶 𝘥𝘦 𝘐𝘈-𝘉𝘰𝘵`,
+                id: `${usedPrefix}menuai`,
+            },
+            {
+                header: "𝗥𝗘𝗗𝗘𝗦 𝗔𝗦𝗨𝗡𝗔 𝗬𝗨𝗨𝗞𝗜 🍄",
+                title: "",
+                description: `𝘔𝘶𝘦𝘴𝘵𝘳𝘢 𝘭𝘢𝘴 𝘳𝘦𝘥𝘦𝘴 𝘥𝘦𝘭 𝘣𝘰𝘵`,
+                id: `${usedPrefix}redes`,
+            },
+            {
+                header: "𝗚𝗥𝗨𝗣𝗢𝗦 ☁️",
+                title: "",
+                description: `𝘔𝘶𝘦𝘴𝘵𝘳𝘢 𝘭𝘰𝘴 𝘨𝘳𝘶𝘱𝘰𝘴 𝘥𝘦𝘭 𝘣𝘰𝘵`,
+                id: `${usedPrefix}grupos`,
+            },
+{
+                header: "𝗩𝗘𝗟𝗢𝗖𝗜𝗗𝗔𝗗 𝗕𝗢𝗧 ⚡",
+                title: "",
+                description: `𝘔𝘶𝘦𝘴𝘵𝘳𝘢 𝘭𝘢 𝘷𝘦𝘭𝘰𝘤𝘪𝘥𝘢𝘥 𝘥𝘦𝘭 𝘣𝘰𝘵`,
+                id: `${usedPrefix}ping`,
+            },
+{
+                header: "𝗗𝗢𝗡𝗔𝗖𝗜𝗢𝗡 𝗕𝗢𝗧 ☕",
+                title: "",
+                description: `𝘔𝘶𝘦𝘴𝘵𝘳𝘢 𝘦𝘭 𝘮𝘦𝘵𝘰𝘥𝘰 𝘥𝘦 𝘥𝘰𝘯𝘢𝘳`,
+                id: `${usedPrefix}donate`,
+            },
+        ],
+    });
+
+    let vid = "https://qu.ax/zUUy.jpeg'";
+    let img = "https://qu.ax/KJrp.jpeg'";
+
+    await conn.sendListB(m.chat, menu, txt, `🌸ᛜॕ༹Mᴇɴᴜ Lɪsᴛ🌸ᛜॕ༹`, [vid, img].getRandom(), listSections, estilo);
+};
+
+handler.command = ["menu", "help", "menú"];
+
+export default handler;
+
+
+function clockString(ms) {
+  const h = Math.floor(ms / 3600000);
+  const m = Math.floor(ms / 60000) % 60;
+  const s = Math.floor(ms / 1000) % 60;
+  console.log({ ms, h, m, s });
+  return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(":");
+}
+
+
+  var ase = new Date();
+  var hour = ase.getHours();
+switch(hour){
+  case 0: hour = 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 🌙'; break;
+  case 1: hour = 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 💤'; break;
+  case 2: hour = 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 🦉'; break;
+  case 3: hour = 'Bᴜᴇɴᴏs Dɪᴀs ✨'; break;
+  case 4: hour = 'Bᴜᴇɴᴏs Dɪᴀs 💫'; break;
+  case 5: hour = 'Bᴜᴇɴᴏs Dɪᴀs 🌅'; break;
+  case 6: hour = 'Bᴜᴇɴᴏs Dɪᴀs 🌄'; break;
+  case 7: hour = 'Bᴜᴇɴᴏs Dɪᴀs 🌅'; break;
+  case 8: hour = 'Bᴜᴇɴᴏs Dɪᴀs 💫'; break;
+  case 9: hour = 'Bᴜᴇɴᴏs Dɪᴀs ✨'; break;
+  case 10: hour = 'Bᴜᴇɴᴏs Dɪᴀs 🌞'; break;
+  case 11: hour = 'Bᴜᴇɴᴏs Dɪᴀs 🌨'; break;
+  case 12: hour = 'Bᴜᴇɴᴏs Dɪᴀs ❄'; break;
+  case 13: hour = 'Bᴜᴇɴᴏs Dɪᴀs 🌤'; break;
+  case 14: hour = 'Bᴜᴇɴᴀs Tᴀʀᴅᴇs 🌇'; break;
+  case 15: hour = 'Bᴜᴇɴᴀs Tᴀʀᴅᴇs 🥀'; break;
+  case 16: hour = 'Bᴜᴇɴᴀs Tᴀʀᴅᴇs 🌹'; break;
+  case 17: hour = 'Bᴜᴇɴᴀs Tᴀʀᴅᴇs 🌆'; break;
+  case 18: hour = 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 🌙'; break;
+  case 19: hour = 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 🌃'; break;
+  case 20: hour = 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 🌌'; break;
+  case 21: hour = 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 🌃'; break;
+  case 22: hour = 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 🌙'; break;
+  case 23: hour = 'Bᴜᴇɴᴀs Nᴏᴄʜᴇs 🌃'; break;
+}
+  var greeting = hour;
