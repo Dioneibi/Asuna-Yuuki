70fa8cd2dca6976950712ca5595bddb4496dc3ab
Dioneibi <ulcidecono@gmail.com>
2024-09-20 17:13:29 -0400

    Update dl-ytmp3.js

--- plugins/dl-ytmp3.js
+++ plugins/dl-ytmp3.js
@@ -1,65 +1,25 @@
-import fg from 'api-dylux'
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
-import yts from 'yt-search'
-import fetch from 'node-fetch' 
-let limit = 100
+import Scraper from "@SumiFX/Scraper"
 
-let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) return m.reply('🔍 Ingresa el enlace del vídeo de YouTube junto al comando.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* https://youtu.be/QSvaCSt8ixs`)
+if (!args[0].match(/youtu/gi)) return conn.reply(m.chat, `Verifica que el enlace sea de YouTube.`, m)
 
-let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-
-if (!args || !args[0]) conn.reply(m.chat, `*⭐ Escribe la URL de un video de YouTube que deseas descargar.*`,  m, fake,)
-if (!args[0].match(/youtu/gi)) return conn.reply(m.chat, `Verifica que la *URL* sea de YouTube`, m).then(_ => m.react('✖️'))
-let q = '128kbps'
-
-await m.react('🕒')
+let user = global.db.data.users[m.sender]
 try {
-const yt = await fg.yta(args[0])
-let { title, dl_url, size } = yt
-let vid = (await yts(text)).all[0]
-let { thumbnail, url } = vid
-
-if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`, m).then(_ => m.react('✖️'))
-
-await m.react('💿')
-await conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: "audio/mp4", fileName: title + '.mp3', quoted: m, contextInfo: {
-'forwardingScore': 200,
-'isForwarded': true,
-externalAdReply:{
-showAdAttribution: false,
-title: `${title}`,
-body: `${vid.author.name}`,
-mediaType: 2, 
-sourceUrl: `${url}`,
-thumbnail: await (await fetch(vid.thumbnail)).buffer()}}}, { quoted: m })
+let { title, size, quality, thumbnail, dl_url } = await Scraper.ytmp4(args[0])
+if (size.includes('GB') || size.replace(' MB', '') > 120) { return await m.reply('El archivo pesa mas de 120 MB, se canceló la Descarga Descargue en MP4DOC PARA QUE DE ENVÍE SU VIDEO.')}
+let txt = ` ╭─⬣「 *YouTube Descarga* 」⬣\n`
+    txt += `│: ̗̀➛ *🔍 Titulo ∙* ${title}\n`
+    txt += `│: ̗̀➛ *🪴 Calidad ∙* ${quality}\n`
+    txt += `│: ̗̀➛ *⚖ Peso ∙* ${size}\n`
+    txt += `╰─⬣`
+await conn.sendFile(m.chat, thumbnail, 'thumbnail.jpg', txt, m)
+await conn.sendFile(m.chat, dl_url, title + '.mp4', `*🔍 Titulo ∙* ${title}\n*🪴 Calidad ∙* ${quality}`, m, false, { asDocument: user.useDocument })
 } catch {
-try {
-let yt = await fg.ytmp3(args[0])
-let { title, size, dl_url } = yt
-let vid = (await yts(text)).all[0]
-let { thumbnail, url } = vid
-
-if (size.split('MB')[0] >= limit) return conn.reply(m.chat,`El archivo pesa mas de ${limit} MB, se canceló la Descarga.`,  m, fake,).then(_ => m.react('✖️'))
-
-await m.react('💿')
-await conn.sendMessage(m.chat, { audio: { url: dl_url }, mimetype: "audio/mp4", fileName: title + '.mp3', quoted: m, contextInfo: {
-'forwardingScore': 200,
-'isForwarded': true,
-externalAdReply:{
-showAdAttribution: false,
-title: `${title}`,
-body: `${vid.author.name}`,
-mediaType: 2, 
-sourceUrl: `${url}`,
-thumbnail: await (await fetch(vid.thumbnail)).buffer()}}}, { quoted: m })
-} catch {
-await conn.reply(m.chat, `*☓ Ocurrió un error inesperado*`,  m, fake,).then(_ => m.react('✖️'))
-console.error(error)
-}}}
-handler.help = ['ytmp3 <url yt>']
-handler.corazones = 2
-handler.tags = ['dl']
-handler.command = /^(fgmp3|dlmp3|getaud|yt(a|mp3))$/i
-handler.star = 2
+}}
+handler.help = ['ytmp4 <yt url>']
+handler.tags = ['downloader']
+handler.command = ['ytmp4', 'yt', 'ytv']
 handler.register = true 
+//handler.limit = 1
 export default handler
